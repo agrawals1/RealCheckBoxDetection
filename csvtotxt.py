c06d83715ffd6c94cbf65339016b18a8690cad62
shubham <shubham.dkhema@gmail.com>
2019-09-09 18:39:08 +0530

    bit of cleaning

--- csvtotxt.py
+++ csvtotxt.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 train_old = pd.DataFrame()
-for file in os.listdir(r"./outputcsv/train/class-0"):
+for file in os.listdir(r"./data/splitcsv/train/class-0"):
 
     train = pd.read_csv(os.path.join(r"./outputcsv/train/class-0", file), index_col=0)
     mystring = (r".\output\train\class-0\{}.png".format(file[:-4]))
@@ -15,5 +15,5 @@ for file in os.listdir(r"./outputcsv/train/class-0"):
 
 
     
-train_old.to_csv(r"./outputtxt/train/class-0/{}.txt".format(file[:-4]), header=None, index=0, sep=' ')
-input("stop")
+train_old.to_csv(r"./data/annotate.txt"), header=None, index=0, sep=' ')
+
